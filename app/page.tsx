3839ea04068cc337becd913ecf1b1c09d5890e96
화준 송 <partner.hwajun@kyobobook.com>
2024-07-19 16:37:47 +0900

    TopNavbar 적용

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import { Layout } from 'antd'
+import TopNavbar from '@/components/molecules/TopNavbar'
+import dynamic from 'next/dynamic'
+const { Header } = Layout
+const Index = () => {
+    return <TopNavbar />
+}
+export default Index
